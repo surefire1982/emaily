6e4137bba55577118c7347e0ca4396a931f433ed
Roi Eclarin <reclarin@gmail.com>
2018-03-28 21:45:28 -0400

    added auth

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys')
+
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' })
-})
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token', accessToken)
+      console.log('refresh token', refreshToken)
+      console.log('profile', profile)
+    }
+  )
+)
+
+// '/auth/google' route will be managed by passport.authenticate
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+)
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 // if PORT env not defined, use default of 5000
 const PORT = process.env.PORT || 5000
