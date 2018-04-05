78bb580dbbe8ddb6c11ffece67600daa353a529e
Roi Eclarin <roi.eclarin@scotiabank.com>
2018-04-04 22:15:08 -0400

    finished auth flow and setup credentials

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// key.js - figure out what set of credentials to return
+if (process.env.NODE_ENV === 'production') {
+  // in production return prod set of keys
+  module.exports = require('./prod')
+} else {
+  // in development, return dev keys
+  module.exports = require('./dev')
+}
