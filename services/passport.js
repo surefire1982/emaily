865698d056086373778b77b918139ee87556f585
Roi Eclarin <reclarin@gmail.com>
2018-04-09 22:42:10 -0400

    added react app and async await

--- services/passport.js
+++ services/passport.js
@@ -25,17 +25,14 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // we already have a record with the given profile ID
-          // no error, returning existing user
-          done(null, existingUser)
-        } else {
-          // no user record with this ID, make a new record
-          new User({ googleId: profile.id }).save().then(user => done(null, user))
-        }
-      })
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id })
+
+      if (existingUser) {
+        return done(null, existingUser)
+      }
+      const user = await new User({ googleId: profile.id }).save()
+      done(null, user)
     }
   )
 )
